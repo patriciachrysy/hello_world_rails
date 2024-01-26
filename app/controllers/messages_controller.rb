bd979f07bcb5956dde5ba86ac5ec411f2fb23716
PATRICIA CHRISTELLE MANEZEU <p.manezeu@group-activa.com>
2024-01-26 21:14:54 +0100

    Added routes and config for the API

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,9 @@
+class MessagesController < ApplicationController
+    def index
+        id = rand(1...5)
+
+        @message = Message.find(id)
+
+        render json: @message, status: :ok
+    end
+end
