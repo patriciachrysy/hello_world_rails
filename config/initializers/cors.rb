150f327ce90c7b244ecb106e169b22ade43d825d
PATRICIA CHRISTELLE MANEZEU <p.manezeu@group-activa.com>
2024-01-27 11:34:08 +0100

    Solved the cors issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,3 +15,16 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins "http://localhost:3001"
+              "http://127.0.0.1:3001"
+  
+      resource "*",
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true,
+        max_age: 86400
+    end
+end
